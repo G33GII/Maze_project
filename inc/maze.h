92e8ce6f555c55e7e1c2cf83e58e199643a577e6
G33GII <gazasolomon@gmail.com>
2024-06-19 16:58:08 +0100

    more dettails to the header file

--- inc/maze.h
+++ inc/maze.h
@@ -1,13 +1,18 @@
 #ifndef MAZE_H
 #define MAZE_H
 
-//Starts up SDL and creates window
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+
+/* Starts up SDL and creates window */
 bool init();
 
-//Loads media
+/* Loads media */
 bool loadMedia();
 
-//Frees media and shuts down SDL
+/* Frees media and shuts down SDL */
 void close();
 
 #endif /* MAZE_H */
