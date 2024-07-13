e0bf11f4298efabec607c40691890bbaf18f4fde
G33GII <gazasolomon@gmail.com>
2024-07-13 15:49:27 +0100

    added function.c and main

--- inc/maze.h
+++ inc/maze.h
@@ -1,18 +1,26 @@
 #ifndef MAZE_H
 #define MAZE_H
 
-
 #include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL_image.h>
 
 
-/* Starts up SDL and creates window */
-bool init();
 
-/* Loads media */
-bool loadMedia();
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define MAP_WIDTH 24
+#define MAP_HEIGHT 24
+#define NUM_TEXTURES 11
 
-/* Frees media and shuts down SDL */
-void close();
 
+
+
+void loadMap(const char *filename);
+void drawMap(SDL_Renderer *renderer);
+void handle_input(bool *quit, double *moveSpeed, double *rotSpeed, int *screenWidth, int *screenHeight);
+void render(SDL_Renderer *renderer, int screenWidth, int screenHeight);
 #endif /* MAZE_H */
